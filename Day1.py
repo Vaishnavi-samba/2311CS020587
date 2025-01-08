a95c4fb067ea218fef0e79dc50808064e8464169
Vaishnavi-samba <vyshnaveesamba9@gmail.com>
2025-01-08 10:34:27 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,11 @@
+# Declare variables of different data types
+integer_var = 42  # Integer
+float_var = 3.14  # Float
+string_var = "Hello, world!"  # String
+boolean_var = True  # Boolean
+
+# Print each variable and its type
+print("Integer variable:", integer_var, "| Type:", type(integer_var))
+print("Float variable:", float_var, "| Type:", type(float_var))
+print("String variable:", string_var, "| Type:", type(string_var))
+print("Boolean variable:", boolean_var, "| Type:", type(boolean_var))
